38969e1577f376cc5b2fd53d8109c62fd284f6cd
tomernahum <git@tomer.nahum.org>
2023-10-21 21:08:59 -0400

    convert to json instead of string

--- app.py
+++ app.py
@@ -3,15 +3,17 @@ from chatgpt import get_response
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 # Define route for home page
+import json
 @app.route("/get", methods=["GET", "POST"])
 def gpt_response():
     userText = request.args.get('msg')
-    return str(get_response(userText))
+    return json.dumps((get_response(userText)))
 
 if __name__ == "__main__":
     app.run(debug=False)
